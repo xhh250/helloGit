3e3ce7125fa0c36792948cbe09a8f0e447ef153b
chenhui <19125688@bjtu.edu.cn>
2021-07-19 21:53:11 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @description:
+ * @author: chenHui
+ * @time: 2021/7/19 21:43
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
